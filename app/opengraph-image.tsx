2a83fce3b7e579f1500c74276c6117c927a967b2
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2024-01-25 22:48:30 -0500

    Try opengraph

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,67 @@
+import { ImageResponse } from "next/og";
+import appIcon from "@/public/avacado.svg";
+// import appImage from "@/public/avacado.png";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  // Font
+  // const interSemiBold = fetch(
+  //   new URL("./Inter-SemiBold.ttf", import.meta.url)
+  // ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="avatar"
+          width="256"
+          src={appIcon}
+          style={{
+            borderRadius: 128,
+          }}
+        />
+        {/* <p>github.com/{username}</p> */}
+        {/* </div> */}
+        {/* {appIcon} */}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      // fonts: [
+      //   {
+      //     name: "Inter",
+      //     data: await interSemiBold,
+      //     style: "normal",
+      //     weight: 400,
+      //   },
+      // ],
+    }
+  );
+}
