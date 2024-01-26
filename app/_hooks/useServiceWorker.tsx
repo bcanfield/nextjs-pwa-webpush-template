7ecde69b992a710e7d2e61657ca0b73a4ee8f421
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2024-01-26 17:16:06 -0500

    cleanup

--- app/_hooks/useServiceWorker.tsx
+++ app/_hooks/useServiceWorker.tsx
@@ -4,10 +4,14 @@ import useUserAgent from "./useUserAgent";
 
 const useServiceWorker = ({ vapidPublicKey }: { vapidPublicKey?: string }) => {
   const { isMobile, isStandalone } = useUserAgent();
+  const [notificationsSupported, setNotificationsSupported] = useState(false);
+  const [userSubscription, setUserSubscription] = useLocalStorage(
+    "user-subscription",
+    ""
+  );
+
   const [notificationsEnabled, setNotificationsEnabled] =
     useState<boolean>(false);
-
-  const [notificationsSupported, setNotificationsSupported] = useState(false);
 
   useEffect(() => {
     setNotificationsSupported(
@@ -18,7 +22,8 @@ const useServiceWorker = ({ vapidPublicKey }: { vapidPublicKey?: string }) => {
     );
   }, []);
 
-  const subscribeToPush = async (reg: ServiceWorkerRegistration) => {
+  // This will retrieve a new subscription from the PushManager that we can tie to a user
+  const subscribeToPushManager = async (reg: ServiceWorkerRegistration) => {
     try {
       const options = {
         applicationServerKey: vapidPublicKey,
@@ -39,10 +44,12 @@ const useServiceWorker = ({ vapidPublicKey }: { vapidPublicKey?: string }) => {
     }
   };
 
+  // This is called when a user clicks a 'Subscribe' button on your site
   const subscribe = async () => {
     if (!notificationsSupported) {
       alert("Notifications not supported in this browser");
     }
+    // Attempt to register the service worker when a user subscribes to notifications
     await navigator.serviceWorker
       .register("/service-worker.js", { scope: "/" })
       .then(
@@ -59,42 +66,27 @@ const useServiceWorker = ({ vapidPublicKey }: { vapidPublicKey?: string }) => {
           }
 
           if (serviceWorker) {
-            console.log("sw current state", serviceWorker.state);
+            // If service worker was already activated, we can simply subscribe to push notifications
             if (serviceWorker.state == "activated") {
-              //If push subscription wasnt done yet have to do here
-              console.log("sw already activated - Do watever needed here");
-              await subscribeToPush(reg);
+              console.log("Service worker was already activated");
+              await subscribeToPushManager(reg);
             }
+            // Otherwise, we will wait for the service worker to activate, request permission, and then subscribe
             serviceWorker.addEventListener("statechange", async function (e) {
               const sw = e.target as ServiceWorker;
-              console.log({ e });
-              console.log("sw statechange : ", sw.state);
               if (sw.state == "activated") {
-                // use pushManger for subscribing here.
-                console.log(
-                  "Just now activated. now we can subscribe for push notification"
-                );
+                console.log("Service worker newly activated");
                 await window?.Notification.requestPermission();
-                const subscribed = await subscribeToPush(reg);
-                console.log({ subscribed });
+                await subscribeToPushManager(reg);
               }
             });
           }
         },
         function (err) {
-          console.error(
-            "unsuccessful registration with ",
-            "/service-worker.js",
-            err
-          );
+          console.error("Unsuccessful service worker registration", err);
         }
       );
   };
-
-  const [userSubscription, setUserSubscription] = useLocalStorage(
-    "user-subscription",
-    ""
-  );
 
   return {
     userSubscription,
